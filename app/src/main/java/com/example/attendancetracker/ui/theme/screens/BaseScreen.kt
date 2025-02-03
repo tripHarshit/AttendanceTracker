cbefaeced0786fe1d7bfe19489a4fb5a73fa6cb0
tripHarshit <1806tripathi@gmail.com>
2025-02-03 20:40:16 +0530

    made till SignUp page

---
+++ app/src/main/java/com/example/attendancetracker/ui/theme/screens/BaseScreen.kt
@@ -0,0 +1,45 @@
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.attendancetracker.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        navController.navigate("login")
+    }
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+       Text(text = "Attendance Tracker",
+           color = Color.Black,
+           fontWeight = FontWeight.Bold,
+           style = MaterialTheme.typography.headlineLarge,
+           modifier = Modifier.padding(16.dp),
+           )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashPreview() {
+    SplashScreen(rememberNavController())
+}
